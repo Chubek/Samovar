3ffa0413ed758cac9dae56efd7a0871b9f371ec6
Chubak <chubakbidpaa@gmail.com>
2022-05-16 15:23:41 +0430

    progess: successfully serves 404

--- chanovar/src/main.rs
+++ chanovar/src/main.rs
@@ -1,15 +1,19 @@
 #[macro_use]
 extern crate macrovar;
 
-use samovar::{samovar::Samovar, request::Request, context::ContextType, common::ResponseTextWrapper};
-
+use samovar::{request::Request, context::ContextType, common::ResponseTextWrapper};
+use samovar::staticserver::StaticServerType;
 
 #[route(method = "GET", path = "/", index = 0, group = "main")]
 fn index(_: &Request, _: &ContextType) -> ResponseTextWrapper {
     ResponseTextWrapper::new("a".to_string())
 }
-#[samovar(addr = "localhost", port = "92", groups = "(main; 0)")]
+#[samovar(addr = "0.0.0.0", port = "8545", groups = "(main; 0)")]
 fn main() {
-    println!("hello world")
+    let fs = StaticServerType::ServeWithoutIndex("static/*".to_string());
+
+    add_new_static_file_server(fs);
+
+    serve_forever();
 
 }
