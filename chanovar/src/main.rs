7be2afd64628f5f8284b83c170849a3b3b1d2781
Chubak <chubakbidpaa@gmail.com>
2022-05-17 12:39:25 +0430

    progress: body parser

--- chanovar/src/main.rs
+++ chanovar/src/main.rs
@@ -1,8 +1,17 @@
 #[macro_use]
 extern crate macrovar;
 
-use samovar::{request::Request, common::{ResponseTextWrapper, DummyResponseType, HttpStatus}, response::Response};
-use serde::{Serialize, Deserialize};
+use std::result;
+
+use samovar::{
+    common::{
+        modify_405_template, DummyResponseType, HttpStatus, MimeType, RequestBodyType,
+        ResponseTextWrapper,
+    },
+    request::{self, Request},
+    response::Response,
+};
+use serde::{Deserialize, Serialize, __private::de::Content};
 
 #[derive(ResponseCommon, Deserialize, Serialize, Clone)]
 struct ResType {
@@ -11,36 +20,60 @@ struct ResType {
     field_u32: u32,
 }
 
-
-#[route(method = "GET", path = "/", index = 0, group = "main")]
+#[route(method = "GET", path = "/")]
 fn index(_: &Request) -> ResponseTextWrapper {
     let mut resp = Response::<DummyResponseType>::new_string(
-              "Ok!".to_string(), 
-     samovar::common::MimeType::TextHtml, 
-          HttpStatus::Http200Ok);
+        "Ok!".to_string(),
+        samovar::common::MimeType::TextHtml,
+        HttpStatus::Http200Ok,
+    );
 
     resp.compose()
 }
 
-#[route(method = "GET", path = "/test", index = 1, group = "main")]
+#[route(method = "POST", path = "/test")]
 fn test(r: &Request) -> ResponseTextWrapper {
     let field_header = r.get_header("test-header").unwrap();
     let field_str = String::from("Served by Samovar");
     let field_u32 = 12u32;
 
-    let res = ResType {field_header, field_str, field_u32};
+    let res = ResType {
+        field_header,
+        field_str,
+        field_u32,
+    };
 
     let status = HttpStatus::Http202Accepted;
 
-    let mut resp = Response::<ResType>::new_json(res, status);
+    let req_body = r.get_body();
 
-    resp.compose()
+    let contents = req_body.content;
+
+    let result = match contents {
+        RequestBodyType::Json(j) => {
+            let mut resp = Response::<DummyResponseType>::new_string(
+                j["aa"].to_string(),
+                MimeType::TextPlain,
+                status,
+            );
+
+            resp.compose()
+        }
+        RequestBodyType::Str(t) => {
+            let mut resp =
+                Response::<DummyResponseType>::new_string(t, MimeType::TextPlain, status);
+
+            resp.compose()
+        }
+    };
+
+    result
 }
 
-#[static_server(glob = "./static/*", path = "/static", group = "static", index_file = "index.html", index = 1)]
-#[samovar(addr = "0.0.0.0", port = "8545", groups = "(main; 0..1), (static; 1)")]
+#[static_server(glob = "./static/*", path = "/static", index_file = "none")]
+#[samovar(addr = "0.0.0.0", port = "1500")]
 fn main() {
+    modify_405_template("This method aint it yo!".to_string());
 
     serve_forever();
-
 }
